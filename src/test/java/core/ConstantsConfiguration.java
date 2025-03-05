c1cbe556b053d6f0913188e5b039d19c1c6e6ccf
leafar-Santos <leafarccomputacao@gmail.com>
2025-03-05 19:09:44 -0300

    Criação da estrutura de testes e criação de testes automatizado

--- src/test/java/core/ConstantsConfiguration.java
+++ src/test/java/core/ConstantsConfiguration.java
@@ -4,9 +4,9 @@ import io.restassured.http.ContentType;
 public interface ConstantsConfiguration {
 
     //public String API_BASE_URL = "https://api-loja-de-livro.onrender.com";
-    //public String API_BASE_URL = "http://localhost:8080";
+    public String API_BASE_URL = "http://localhost:8080";
 
-    public String API_BASE_URL = null;
+    //public String API_BASE_URL = null;
     public Integer API_PORT = 443;
     public String API_BASE_PATH = "";
     public ContentType API_CONTENT_TYPE = ContentType.JSON;
